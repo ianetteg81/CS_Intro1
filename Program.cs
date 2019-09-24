1a082932fc9f47b3de7997e1a35b324e0a6a117e
ianetteg81 <54339016+ianetteg81@users.noreply.github.com>
2019-09-24 11:07:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Cs_intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            // String Variable
+            string innovations = "Never Stop Creating";
+
+            // Number Variables
+            int num1 = 95;
+            float num2 = 95.775f;
+            double num3 = 95.755;
+
+            // Boolean Variable
+            bool boolean = true;
+
+            Console.WriteLine(innovations);
+            Console.WriteLine(num2);
+            Console.WriteLine(boolean);
+
+            // Press key to continue
+            Console.ReadKey();
+
+            // Concatenation
+            string concat1 = "Be" + " " + "Brilliant";
+            string concat2 = "Be" + " " + "Bright";
+
+            // Math
+            float out1 = 9.9f * 30f;
+            float out2 = 4.5f * 15f;
+
+            //Constants
+            const string myName = "Ianette";
+                const bool arrow = true;
+
+            // String interpolation
+            Console.WriteLine("myName is {0}. Num: {1}, Num: {2}.", myName, num1, out2);
+        }
+    }
+}
